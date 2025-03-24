56c1f884a3596b3642b5b19ab093a73f1298fb83
NABARUN <nbiswas100@outlook.com>
2025-03-25 04:40:50 +0530

    remove vercel

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,6 @@
 import path from "path";
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -10,8 +10,4 @@ export default defineConfig({
       "@": path.resolve(__dirname, "./src"),
     },
   },
-  base: './', // Ensures correct relative paths for assets
-  build: {
-    outDir: 'dist', // Ensures Vercel deploys the correct folder
-  },
-})
+});
